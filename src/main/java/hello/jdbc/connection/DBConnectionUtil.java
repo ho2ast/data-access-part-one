9d4fcfe77cb04e05df6888f0c9c7d4268241cca9
HO2AST <lucid7@hotmail.co.kr>
2022-09-17 02:10:37 +0900

    connection pool, datasource

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -1,6 +1,7 @@
 package hello.jdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,54 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+
+    @Test
+    void dateSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        userDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // connection pooling
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void userDataSource(DataSource dataSource) throws SQLException {
+        Connection connection1 = dataSource.getConnection();
+        Connection connection2 = dataSource.getConnection();
+
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+}
